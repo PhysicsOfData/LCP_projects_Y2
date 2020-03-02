feae5be325b9accbc79532ef58ac4ad228c3fea8
Dan8991 <daniseas98@gmail.com>
2020-03-02 15:40:02 +0100

    [Add]improved plots

--- utility_functions/graphics.py
+++ utility_functions/graphics.py
@@ -5,17 +5,22 @@ import matplotlib.pyplot as plt
 ### pkt_plot function
 #This function takes in input the times in which packets are recepted at destination and the transmission time
 #It enables us to visualize the moments in which we receive packets
-def pkt_plot(packets,t_tx):
+def pkt_plot(packets, t_tx, axs = None):
     recept_times = [x.arrival_time for x in packets]
     lines=[[(x,0),(x+t_tx,0)] for x in recept_times]
     cmap=["snow","blue","coral","yellow","green","black","rebeccapurple","sienna","navy","slategray"]
     colors=[cmap[x.source] for x in packets]
     lc = mc.LineCollection(lines, linewidths=10,color=colors)
-    _, ax = plt.subplots()
-    ax.add_collection(lc)
+
+    if axs is None:
+        _, axs = plt.subplots()
+
+    axs.add_collection(lc)
     
-    ax.set_xlim(0,(recept_times[-1]+t_tx)*1.01)
-    ax.set_ylim(-0.5,0.5)
-    ax.set_yticks([])
+    axs.set_xlim(0,(recept_times[-1]+t_tx)*1.01)
+    axs.set_ylim(-0.5,0.5)
+    axs.set_yticks([])
+    axs.set_xlabel("Time(s)")
+    axs.set_title("Arrival time to earth of each packet")
     
     plt.show()
